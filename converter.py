9fd5e7ee327de973bc727beff1087d76186bd4ef
ramosdeluis <alberto.rir@icloud.com>
2022-12-10 21:16:04 -0300

    Creating the calculator and ending the code.

--- converter.py
+++ converter.py
@@ -1,8 +1,22 @@
+from data import data
+
+
 class Converter:
     def __init__(self, user_value, choices):
-        self.user_value = user_value
-        self.choices = choices
-        self.data = {}
+        self.user_value: float = user_value
+        self.choices: dict = choices
+        self.data: dict = data
 
-    def calculate(self):
-        pass
+    def calculate(self) -> float:
+        if self.choices['in'] == self.choices['out']:
+            return self.user_value
+        else:
+            result = self.data[self.choices['in']][self.choices['out']] * self.user_value
+            return result
+
+    def reverse_calculate(self) -> float:
+        if self.choices['in'] == self.choices['out']:
+            return self.user_value
+        else:
+            result = self.user_value / self.data[self.choices['out']][self.choices['in']]
+            return result
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 
+from converter import Converter
+
 # Creating the Window.
 window = Tk()
 window.title('Units Converter')
@@ -30,7 +32,7 @@ def listbox_select_out(event):
 list_box_in = Listbox(width=10, exportselection=0)
 list_box_out = Listbox(width=10, exportselection=0)
 units_list = ['millimeter', 'centimeter', 'decimeter', 'meter', 'decameter', 'hectometer',
-              'kilometer', 'foot', 'yard', 'rod', 'chain', 'furlong', 'mile']
+              'kilometer', 'inches', 'feet', 'yard', 'rod', 'chain', 'furlong', 'mile']
 for item in units_list:
     list_box_in.insert(units_list.index(item), item)
     list_box_out.insert(units_list.index(item), item)
@@ -55,7 +57,15 @@ def click_button():
     user_number = entry.get()
     try:
         user_number = float(user_number)
-        label_tree.config(text=f"{user_number}")
+        converter = Converter(user_number, choices)
+        try:
+            label_four.config(text=f"{converter.user_value} {converter.choices['in']}\n=")
+            label_four.grid(column=1, columnspan=3, row=5)
+            label_tree.config(text=f"{converter.calculate():.4f} "
+                                   f"{converter.choices['out']}s")
+        except:
+            label_tree.config(text=f"{converter.reverse_calculate():.4f} "
+                                   f"{converter.choices['out']}s")
     except ValueError:
         label_tree.config(text=f"Sorry, '{user_number}' \nis not an option.")
 
@@ -70,7 +80,8 @@ convert_button.grid(column=1, columnspan=3, row=4)
 # Output interface.
 
 label_tree = Label(text='Awaiting the command...', font=("Comic Sans MS", 15, "bold"))
-label_tree.config(padx=10, pady=20)
-label_tree.grid(column=1, columnspan=3, row=5)
+label_tree.grid(column=1, columnspan=3, row=6)
+
+label_four = Label(font=("Comic Sans MS", 15, "bold"))
 
 window.mainloop()
